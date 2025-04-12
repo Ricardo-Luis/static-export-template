798f5248093e9a78bd99757ac4f5f26bc77d3aee
Ricardo Luís <65141488+Ricardo-Luis@users.noreply.github.com>
2025-04-12 20:18:43 +0100

    Add files via upload

---
+++ teste_linux.jl
@@ -0,0 +1,19 @@
+### A Pluto.jl notebook ###
+# v0.20.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 9c03523a-179d-11f0-0e12-d36a97d1ace2
+using EasyFit
+
+# ╔═╡ 9c008a2d-c79a-420c-bbd2-67e57e6f81e9
+using Dierckx
+
+# ╔═╡ 382f98c0-8e5d-4506-bf3c-be1d25f8d364
+using SankeyPlots
+
+# ╔═╡ Cell order:
+# ╠═9c03523a-179d-11f0-0e12-d36a97d1ace2
+# ╠═9c008a2d-c79a-420c-bbd2-67e57e6f81e9
+# ╠═382f98c0-8e5d-4506-bf3c-be1d25f8d364
